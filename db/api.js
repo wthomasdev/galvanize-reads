fc007f88b59b19bcd91a925d6653ee72c20c1b8e
Will Thomas <wlthomas001@gmail.com>
2016-07-12 13:19:33 -0600

    add book

--- db/api.js
+++ db/api.js
@@ -3,6 +3,9 @@ var knex = require('./knex');
 module.exports = {
   findBooks: function () {
     return knex('book').select();
+  },
+  addBook: function (data) {
+    return knex('book').insert(data);
   }
 
 
--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,22 @@ var db = require('../db/api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.findBooks().then(function(data) {
-    res.render('books', { book:data});
-
-  })
+	db.findBooks().then(function(data) {
+		res.render('books', {
+			book: data
+		});
+	})
 });
+
+router.get('/add', function(req, res, next) {
+	res.render('addbook')
+});
+
+router.post('/add', function(req, res, next) {
+	console.log(req.body);
+	db.addBook(req.body).then(function() {
+		res.redirect('/books');
+	})
+})
 
 module.exports = router;
